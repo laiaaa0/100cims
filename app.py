db6ffff1f1c75b9c3d308daddb4029b158af8afb
Laia Freixas <lfreixas@oni.bio>
2024-11-05 19:18:34 +0000

    check black

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ from summit import summit_list
 import os
 
 
-class Handler():
+class Handler:
     def __init__(self):
         token = os.getenv("CIMS_BOT_TOKEN")
         if not token:
             raise Exception("Please define CIMS_BOT_TOKEN environment variable")
-        self.summits = summit_list.load_from_file(os.path.join(os.path.dirname(__file__), "test","100cims.csv"))
+        self.summits = summit_list.load_from_file(
+            os.path.join(os.path.dirname(__file__), "test", "100cims.csv")
+        )
         print(self.summits.size())
         self.bot = telepot.Bot("7506903633:AAHgK3aSqraCMAaJN3DD99u1vROB34nke7c")
         self.bot.getMe()
         self.bot.message_loop(self.handle)
-
 
     def command_from_text(self, text):
         args_list = text.split()
@@ -31,15 +32,15 @@ class Handler():
 
     def handle(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
-        if content_type != 'text':
+        if content_type != "text":
             self.bot.sendMessage(chat_id, "This is not a command!")
             return
-        chat_id = msg['chat']['id']
-        chat_text = msg['text']
+        chat_id = msg["chat"]["id"]
+        chat_text = msg["text"]
         command = self.command_from_text(chat_text)
-        print('Got command %s' % command)
-        print('From ID ', chat_id)
-        if command == '/closest':
+        print("Got command %s" % command)
+        print("From ID ", chat_id)
+        if command == "/closest":
             # TODO get limit (num) and get limit (distance)
             MAX_DIST = 30
             location = self.rest_of_message(chat_text)
@@ -49,18 +50,13 @@ class Handler():
             self.bot.sendMessage(chat_id, message_from_list)
 
 
-
-
 if __name__ == "__main__":
     h = Handler()
 
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
-        handlers=[
-            logging.StreamHandler(),
-            logging.FileHandler("telegram_bot.log")
-        ]
+        handlers=[logging.StreamHandler(), logging.FileHandler("telegram_bot.log")],
     )
 
     while True:
